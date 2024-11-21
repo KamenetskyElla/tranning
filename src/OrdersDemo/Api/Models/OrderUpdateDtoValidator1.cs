495f6a04a874460ebf33398346c11b1efa6bdaf2
Ella Kamenetsky <ella.kamenetsky@passportcard.co.il>
2024-11-21 18:23:13 +0200

    Validation

---
+++ src/OrdersDemo/Api/Models/OrderUpdateDtoValidator1.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace OrdersDemo.Api.Models;
+
+public class OrderUpdateDtoValidator1 : AbstractValidator<OrderUpdateDto>
+{
+    public OrderUpdateDtoValidator1()
+    {
+        RuleFor(x => x.City).NotEmpty();//.GreaterThan(0);
+                                        // RuleFor(x => x.Street)//.Validate();
+    }
+}
+
