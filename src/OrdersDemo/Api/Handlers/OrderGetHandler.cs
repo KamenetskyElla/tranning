6f8fae99573fa2fcdce3b42501ba1864a9b02a0c
Ella Kamenetsky <ella.kamenetsky@passportcard.co.il>
2024-11-24 11:30:06 +0200

    add spec

--- src/OrdersDemo/Api/Handlers/OrderGetHandler.cs
+++ src/OrdersDemo/Api/Handlers/OrderGetHandler.cs
@@ -4,6 +4,7 @@ using MediatR;
 using OrdersDemo.Api.Models;
 using OrdersDemo.Domain;
 using OrdersDemo.Repository;
+using OrdersDemo.Repository.Spec;
 
 namespace OrdersDemo.Api.Handlers;
 public record OrderGetRequest(int Id) : IRequest<OrderDto>;
@@ -12,8 +13,12 @@ public class OrderGetHandler(IReadRepository<Order> readRepository, IMapper mapp
     public async Task<OrderDto> Handle(OrderGetRequest request, CancellationToken cancellationToken)
     {
         var id = request.Id;
-        var order = await readRepository.GetByIdAsync(id, cancellationToken);
+        var spec = new OrderSpec(request.Id);
+        var order = await readRepository.GetBySpecAsync(spec, cancellationToken);
         Guard.Against.NotFound(id, order);
         return mapper.Map<OrderDto>(order);
+        //var order = await readRepository.GetByIdAsync(id, cancellationToken);
+        //Guard.Against.NotFound(id, order);
+        //return mapper.Map<OrderDto>(order);
     }
 }
